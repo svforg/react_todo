87ea8c8810c86dbc3c5b9067790d86054af83cc4
svforg <svforgel89@gmail.com>
2020-12-22 17:26:55 +0500

    Updates

---
+++ precache-manifest.2c769fc4f63a4809e126a365a2601cc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5044c2791c0c58f100227d95c1df42eb",
+    "url": "/react_todo/index.html"
+  },
+  {
+    "revision": "1ba89e970b518178e583",
+    "url": "/react_todo/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2a16335d757a75444c22",
+    "url": "/react_todo/static/js/2.504642c6.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/react_todo/static/js/2.504642c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ba89e970b518178e583",
+    "url": "/react_todo/static/js/main.8df8739b.chunk.js"
+  },
+  {
+    "revision": "f0f7b3c8281a6f41d057",
+    "url": "/react_todo/static/js/runtime-main.f3caa06c.js"
+  }
+]);
